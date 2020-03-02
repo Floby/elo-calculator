e070e04507aa14c46776c10575a432103221fadc
Florent Jaby <florent.jaby@gmail.com>
2020-03-02 15:24:38 +0100

    Do what CORS require

--- src/application.js
+++ src/application.js
@@ -11,7 +11,8 @@ const app = express();
 app.use(express.json());
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methodis', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   next()
 })
 
